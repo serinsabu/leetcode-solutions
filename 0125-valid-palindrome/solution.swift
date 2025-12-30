5df0f1d6b80d44e68194b140b2f063bb8c83264c
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2025-12-30 15:16:28 +0000

    Sync LeetCode submission Runtime - 11 ms (86.45%), Memory - 20.2 MB (78.45%)

--- 0125-valid-palindrome/solution.swift
+++ 0125-valid-palindrome/solution.swift
@@ -1,27 +1,29 @@
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
+        var n = s.count
         var i=0
-        var j=s.count-1;
+        var j=n-1
 
         var chars = Array(s)
 
         while(i < j) {
             if !chars[i].isLetter && !chars[i].isNumber {
-               i += 1
-               continue 
-            }
-
+                i += 1
+                continue
+            } 
+            
             if !chars[j].isLetter && !chars[j].isNumber {
                 j -= 1
                 continue
             }
 
             if chars[i].lowercased() != chars[j].lowercased() {
-                return false;
+                return false
             }
-            i += 1
-            j -= 1
+
+                i += 1
+                j -= 1
         }
-        return true;
+        return true
     }
 }
