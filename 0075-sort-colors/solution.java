53892eecada3287d82a5765df21c6e472f22b178
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-01-11 13:03:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (42.55%)

--- 0075-sort-colors/solution.java
+++ 0075-sort-colors/solution.java
@@ -1,31 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
-        int i=0;
-        int j=n-1;
-        int k=0;
+        int low=0;
+        int high=n-1;
+        int mid=0;
 
-        while(k <= j) {
-            if(nums[k] == 1) {
-                // do nthng and just move k, as k must have all 1's
-                k++;
-            } else if(nums[k] == 2) {
-                // j pos must have 2, so swap with nums[k]
-                int temp = nums[j];
-                nums[j] = nums[k];
-                nums[k] = temp;
-                j--; // dec j as we know that j got its value 2 at the end
-                // do not move k as k pos can have any value 0,1,2 
-                // it is not necessary that it can have 1 only
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                //swap low with mid
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                mid++;
+                low++;
+            } else if(nums[mid] == 1) {
+                mid++;
             } else {
-                // when nums[k] == 0
-                // when i < k or i == k  and nums[k] == 0 , move i and k (reason in notes)
-                // swap nums[i] with nums[k]
-                int temp = nums[i];
-                nums[i] = nums[k];
-                nums[k] = temp;
-                i++;
-                k++;
+                //swap high with mid
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
             }
         }
     }
