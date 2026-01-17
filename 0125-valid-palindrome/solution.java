745f906b3e103505ed0358f065e273471a6a17bc
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-01-17 17:18:29 +0000

    Sync LeetCode submission Runtime - 2 ms (99.24%), Memory - 44.2 MB (84.45%)

--- 0125-valid-palindrome/solution.java
+++ 0125-valid-palindrome/solution.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        //if we find non alphanumeric character at i, then i++, if at j then j--
+        int n = s.length();
         int i=0;
-        int j=s.length()-1;
+        int j=n-1;
 
         while(i < j) {
-            // check non alpha numeric
-            char chLeft = s.charAt(i);
-            char chRight = s.charAt(j);
-            if(!Character.isLetterOrDigit(chLeft)) {
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+
+            if(!Character.isLetter(ch1) && !Character.isDigit(ch1)) {
                 i++;
                 continue;
             }
-            
-            if(!Character.isLetterOrDigit(chRight)) {
+            if(!Character.isLetter(ch2) && !Character.isDigit(ch2)) {
                 j--;
                 continue;
             } 
-
-            if(Character.toLowerCase(chLeft) != Character.toLowerCase(chRight)) {
-                return false;
-            } 
+                if(Character.toLowerCase(ch1) == Character.toLowerCase(ch2)) {
+                    i++;
+                    j--;
+                } else {
+                    return false;
+                }
             
-                i++;
-                j--;
         }
         return true;
     }
