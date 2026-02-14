54bb4fc4d6566675ba8939db2e10f4de8d1df8f3
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-02-14 18:40:57 +0000

    Sync LeetCode submission Runtime - 2 ms (88.76%), Memory - 48.2 MB (9.07%)

--- 0055-jump-game/solution.java
+++ 0055-jump-game/solution.java
@@ -3,15 +3,12 @@ class Solution {
         int n = nums.length;
         int maxReach = 0;
 
-        /*maxReach = farthest index reachable so far
-        If you ever reach an index you can’t get to, the answer is false
-        */
-
         for(int i=0; i<n; i++) {
             if(i > maxReach) {
                 return false;
+            } else {
+                maxReach = Math.max(maxReach, i+nums[i]);
             }
-            maxReach = Math.max(maxReach, i+nums[i]);
         }
         return true;
     }
