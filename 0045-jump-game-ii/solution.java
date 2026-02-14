08736b33b32540dce1554c596bdf819b85dda7f0
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-02-14 19:12:20 +0000

    Sync LeetCode submission Runtime - 1 ms (99.71%), Memory - 47.6 MB (8.56%)

--- 0045-jump-game-ii/solution.java
+++ 0045-jump-game-ii/solution.java
@@ -1,17 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
-        int currEnd = 0;
-        int farthest = 0;
-        int jumps = 0;
-
         int n = nums.length;
+        int jumps = 0;
+        int curr = 0;
+        int maxReach = 0;
 
-        for(int i=0; i<n-1; i++) {
-            farthest = Math.max(farthest, i+nums[i]);
-            if(i == currEnd) {
-                jumps++;
-                currEnd = farthest;
-            }
+        for(int i=0; i<n-1; i++) {//4
+            maxReach = Math.max(maxReach, i+nums[i]);//4
+            if(i == curr) {
+                jumps++;//3
+                curr = maxReach;//4
+            } 
         }
         return jumps;
     }
