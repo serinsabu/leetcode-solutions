99108337e58f86988ad2b66e7aca846debaa238f
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-02-17 16:56:50 +0000

    Sync LeetCode submission Runtime - 1 ms (99.89%), Memory - 55.8 MB (17.66%)

--- 0169-majority-element/solution.java
+++ 0169-majority-element/solution.java
@@ -1,20 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        int candidate = nums[0];
-        int count = 1;
+        int count = 0;
+        int candidate = 0;
 
-        for(int i=1; i<n; i++) {
-            if(nums[i] == candidate) {
-                count++;
-            } else {
-                count--;
-            }
-
+        for(int i=0; i<n; i++) {
+            // 3 cases
+            // if count == 0, update current elt as new candidate
+            // set count = 1 , becoz no candidate is winning
             if(count == 0) {
                 candidate = nums[i];
                 count = 1;
-            }
+            } else if(nums[i] == candidate) {
+                // if curr == count , increase count, supporting the candidate
+                count++;
+            } else {
+                // if curr != count, decrease count, cancellation of votes
+                count--;
+            }    
         }
         return candidate;
     }
