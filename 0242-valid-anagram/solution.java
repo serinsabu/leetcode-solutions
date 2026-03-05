a4f8252d79acfcb3cc878c81b1946f7db1e296b1
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-03-05 17:13:19 +0000

    Sync LeetCode submission Runtime - 16 ms (21.03%), Memory - 46.7 MB (16.08%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,31 +1,31 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // anagram => must contain every character of t in s, order can be different
-        /*
-            Two strings are anagrams if and only if:
-            They have the same length
-            Every character appears the same number of times
-            so check length first
-        */
-        int[] freq = new int[256];
         int n = s.length();
         int m = t.length();
 
-        if(n != m) return false;
-
-        for(int i=0; i<n; i++) {
-            char ch1 = s.charAt(i);
-            freq[ch1]++;
+        Map<Character, Integer> map1 = new HashMap<>();
+        //put all characters of t
+        for(int i=0; i<m; i++) {
+            char ch = t.charAt(i);
+            //a-3,n-1,m-1,g-1,r-1
+            map1.put(ch, map1.getOrDefault(ch, 0)+1);
         }
 
-        for(int i=0; i<m; i++) {
-            char ch1 = t.charAt(i);
-            if(freq[ch1] > 0) {
-                freq[ch1]--;
+        for(int i=0; i<n; i++) {
+            char ch = s.charAt(i);
+
+            if(map1.containsKey(ch)) {
+                if(map1.get(ch) > 1) {
+                    map1.put(ch, map1.getOrDefault(ch, 0)-1);
+                } else {
+                    map1.remove(ch);
+                }
             } else {
                 return false;
             }
+            //a-3,n-1,m-1,g-1,r-1
+            // map1.put(ch, map.getOrDefault(ch, 0)+1);
         }
-        return true;
+        return map1.size() == 0;
     }
 }
