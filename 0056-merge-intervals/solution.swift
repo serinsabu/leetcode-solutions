bb381c171efc11f1d15fe55b88b670506801199f
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-03-15 06:49:53 +0000

    Sync LeetCode submission Runtime - 2 ms (53.07%), Memory - 20.1 MB (94.83%)

--- 0056-merge-intervals/solution.swift
+++ 0056-merge-intervals/solution.swift
@@ -1,24 +1,34 @@
 class Solution {
     func merge(_ intervals: [[Int]]) -> [[Int]] {
-        guard !intervals.isEmpty else { return [] }
-        var sorted = intervals.sorted{$0[0] < $1[0] }//sort by start time
-        var ans = [sorted[0]]
+        var ans = [[Int]]()
+        // sort intervals by start time 
+        let sorted = intervals.sorted {$0[0] < $1[0]}
+        var s1 = sorted[0][0]
+        var e1 = sorted[0][1]
+        for interval in 1..<sorted.count {
+            var s2 = sorted[interval][0]
+            var e2 = sorted[interval][1]
 
-        for i in 1..<sorted.count {
-            var prev = ans.removeLast()//[s1,e1]
-            var curr = sorted[i]//[s2,e2]
-
-            //overlapping
-            if curr[0] <= prev[1] {
-                // update e1
-                prev[1] = max(curr[1], prev[1])
-                ans.append(prev)
+            // overlapping -> When intervals overlap, you should extend the end, not change start.
+            /*
+            if overlapping
+                extend end
+            else
+                store previous interval
+                start new interval
+            */
+            if s2 <= e1 {
+                // update
+                e1 = max(e1, e2)
             } else {
-                // non overlapping -> merge
-                ans.append(prev)
-                ans.append(curr)
+                // merge overlapped intervals during non overlap
+                ans.append([s1, e1])
+                s1 = s2
+                e1 = e2
             }
         }
+        // merge last interval
+        ans.append([s1, e1])
         return ans
     }
 }
