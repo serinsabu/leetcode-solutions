d093d82cc080ac4089c1cbb35cfc629491b10198
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-04-05 16:55:05 +0000

    Sync LeetCode submission Runtime - 44 ms (100.00%), Memory - 26.7 MB (100.00%)

--- 2626-count-the-number-of-good-subarrays/solution.swift
+++ 2626-count-the-number-of-good-subarrays/solution.swift
@@ -1,22 +1,20 @@
 class Solution {
     func countGood(_ nums: [Int], _ k: Int) -> Int {
-        var n = nums.count
-        var i=0
-        var j=0
-        var map: [Int: Int] = [:]
+        var dict = [Int: Int]()
         var count = 0
+        var n = nums.count
+        var i = 0
+        var j = 0
         var pairs = 0
 
         while j < n {
-            // Then update the frequency.
-            // This avoids recomputing combinations every time.
-            pairs += map[nums[j], default: 0]
-            //count pairs
-            map[nums[j], default:0] += 1
+            pairs += dict[nums[j], default: 0]
+            dict[nums[j], default: 0] += 1
+
             while pairs >= k {
-                count += n-j
-                map[nums[i], default:0] -= 1
-                pairs -= map[nums[i], default: 0]
+                count += n - j
+                dict[nums[i], default: 0] -= 1
+                pairs -= dict[nums[i], default: 0]
                 i += 1
             }
             j += 1
