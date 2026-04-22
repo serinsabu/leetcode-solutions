dd4f0d1867fce708f0c5b7dee375280ab0fee7ed
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-04-22 16:46:16 +0000

    Sync LeetCode submission Runtime - 130 ms (76.29%), Memory - 21.1 MB (60.82%)

--- 0200-number-of-islands/solution.swift
+++ 0200-number-of-islands/solution.swift
@@ -1,40 +1,40 @@
 class Solution {
     func numIslands(_ grid: [[Character]]) -> Int {
+        var m = grid.count//rows
+        var n = grid[0].count//columns
         var grid = grid
-        let m = grid.count
-        let n = grid[0].count
+        var count = 0
 
-        // matrix dfs => x,y
-        func dfs(_ x: Int, _ y: Int) {
-            guard grid[x][y] == "1" else {
-                return 
-            }
-            // make grid visited
-            grid[x][y] = "2"
-            //tuple for directions top, bottom, right, left
-            var dirs = [(0,1),(1,0),(-1,0),(0,-1)]
-
-            for (dx,dy) in dirs {
-                let nx = x + dx
-                let ny = y + dy
-
-                // condition check for out of bounds and visited or not
-                guard nx >= 0, nx < m, ny >= 0, ny < n, grid[nx][ny] == "1" else {
-                    continue
-                } 
-
-                dfs(nx, ny)
-            }
-        }
-        var islandCount = 0
+        // if the grid[i][j] == '1' then start dfs from there
+        // if dfs is done for one island then increase count
         for i in 0..<m {
             for j in 0..<n {
                 if grid[i][j] == "1" {
                     dfs(i, j)
-                    islandCount += 1
+                    count += 1
                 }
             }
         }
-        return islandCount
+
+        func dfs(_ r: Int, _ c: Int) {
+            // mark it visited
+            grid[r][c] = "0"
+
+            //4 directions
+            let dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+
+            for (dx,dy) in dirs {
+                let newR = r + dx
+                let newC = c + dy
+                //boundary check
+
+                guard newR >= 0, newR < m, newC >= 0, newC < n, grid[newR][newC] == "1" else {
+                    continue
+                } 
+
+                dfs(newR, newC)
+            }
+        }
+        return count
     }
 }
