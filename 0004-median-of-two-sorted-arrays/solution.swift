0eca409cf7b0b0144ccf4e7c3872f3d3a0ceb49d
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-05-09 16:53:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.8 MB (7.77%)

--- 0004-median-of-two-sorted-arrays/solution.swift
+++ 0004-median-of-two-sorted-arrays/solution.swift
@@ -1,42 +1,44 @@
 class Solution {
     func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
-
-        // nums1 is smaller array
+        
+        // take the smaller array and do binary search on them
         let A = nums1.count <= nums2.count ? nums1 : nums2
         let B = nums1.count <= nums2.count ? nums2 : nums1
 
-        var n = A.count
-        var m = B.count
+        var m = A.count
+        var n = B.count
 
-        // Binary search is on nums1.
         var low = 0
-        var high = n
+        var high = m
 
         while low <= high {
-            var partition1 = (low+high)/2
-            var partition2 = (m+n+1)/2 - partition1
+            var partitionA = (low+high)/2
+            //total left size fixed
+            var partitionB = (m+n+1)/2 - partitionA
 
-            var leftA = partition1 == 0 ? Int.min : A[partition1 - 1]
-            var rightA = partition1 == n ? Int.max : A[partition1]
-            var leftB = partition2 == 0 ? Int.min : B[partition2 - 1]
-            var rightB = partition2 == m ? Int.max : B[partition2]
-            // correct partition - direct find median for odd or even length
+            var leftA = partitionA == 0 ? Int.min : A[partitionA-1]
+            var rightA = partitionA == m ? Int.max : A[partitionA]
+
+            var leftB = partitionB == 0 ? Int.min : B[partitionB-1]
+            var rightB = partitionB == n ? Int.max : B[partitionB]
+
+            // find median
             if leftA <= rightB && leftB <= rightA {
-                
-                // odd length
+                // total == odd length
                 if (m+n)%2 == 1 {
-                    //because left side contributes maximum.
                     return Double(max(leftA, leftB))
-                } else {
-                    // even length
-                    return Double((max(leftA, leftB) + min(rightA, rightB)))/2.0
-                }
-            } else if leftA > rightB { // wrong partition
-                // move left need more from nums1, move relative to partition
-                high = partition1 - 1
+                } else { // even length
+                    return Double(max(leftA, leftB) + min(rightA, rightB))/2.0
+                }   
+            } 
+            //leftA too small → move right
+            //leftA too big → move left
+            else if leftA < rightB {
+                // move right
+                low = partitionA + 1
             } else {
-                // move right need more from nums2
-                low = partition1 + 1
+                // move left
+                high = partitionA - 1
             }
         }
         return 0.0
