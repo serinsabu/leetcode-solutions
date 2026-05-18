4df54909791091b16b7bdb8505ce982c6370ab92
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-05-18 18:07:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.4 MB (22.98%)

--- 0004-median-of-two-sorted-arrays/solution.swift
+++ 0004-median-of-two-sorted-arrays/solution.swift
@@ -1,20 +1,30 @@
 class Solution {
     func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
-        
-        // take the smaller array and do binary search on them
-        let A = nums1.count <= nums2.count ? nums1 : nums2
-        let B = nums1.count <= nums2.count ? nums2 : nums1
 
-        var m = A.count
-        var n = B.count
+        // 1. Binary search smaller array
+        // 2. Guess partitionA
+        // 3. Compute partitionB
+        // 4. Find boundary elements
+        // 5. Check partition validity
+        // 6. leftA too big → move left
+        // 7. leftB too big → move right
+        // 8. valid partition → compute median
 
+        // choose left array or right array - smaller array
+        var A = nums1.count <= nums2.count ? nums1 : nums2
+        var B = nums1.count <= nums2.count ? nums2 : nums1
+
+        let m = A.count
+        let n = B.count
+
+        //we binary search ALL possible cut positions
         var low = 0
         var high = m
 
         while low <= high {
-            var partitionA = (low+high)/2
-            //total left size fixed
-            var partitionB = (m+n+1)/2 - partitionA
+            let partitionA = (low + high)/2
+            //totalLeft = (m+n+1)/2
+            let partitionB = (m+n+1)/2 - partitionA
 
             var leftA = partitionA == 0 ? Int.min : A[partitionA-1]
             var rightA = partitionA == m ? Int.max : A[partitionA]
@@ -22,22 +32,20 @@ class Solution {
             var leftB = partitionB == 0 ? Int.min : B[partitionB-1]
             var rightB = partitionB == n ? Int.max : B[partitionB]
 
-            // find median
+            //ALL left elements <= ALL right elements
             if leftA <= rightB && leftB <= rightA {
-                // total == odd length
+                // compute
+                // odd elements
                 if (m+n)%2 == 1 {
                     return Double(max(leftA, leftB))
-                } else { // even length
+                } else {
+                    // even elements
                     return Double(max(leftA, leftB) + min(rightA, rightB))/2.0
-                }   
-            } 
-            //leftA too small → move right
-            //leftA too big → move left
-            else if leftA < rightB {
-                // move right
+                }
+            } else if leftA < rightB {
+                // left too big-> A contributed TOO MANY elements
                 low = partitionA + 1
             } else {
-                // move left
                 high = partitionA - 1
             }
         }
