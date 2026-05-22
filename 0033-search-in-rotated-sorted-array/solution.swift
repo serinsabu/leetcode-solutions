8b2800fea8eac149eb80d4d1c6dbe648846c5f75
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-05-22 12:49:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.8 MB (44.11%)

--- 0033-search-in-rotated-sorted-array/solution.swift
+++ 0033-search-in-rotated-sorted-array/solution.swift
@@ -1,26 +1,30 @@
 class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
+        var n = nums.count
         var low = 0
-        var high = nums.count-1
+        var high = n-1
 
         while low <= high {
             var mid = (low+high)/2
             if target == nums[mid] {
                 return mid
             }
+            
             if nums[low] <= nums[mid] {
-                if target >= nums[low] && target <= nums[mid] {
+                // left half sorted
+                if target >= nums[low] && target < nums[mid] {
                     high = mid-1
                 } else {
                     low = mid+1
                 }
             } else {
-                if target >= nums[mid] && target <= nums[high] {
+                // right half sorted
+                if target > nums[mid] && target <= nums[high] {
                     low = mid+1
                 } else {
                     high = mid-1
                 }
-            }
+            } 
         }
         return -1
     }
