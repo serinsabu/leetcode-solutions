8b2fdcc45e0363df84c4543b627b7c9d13b70ad8
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-05-27 03:38:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.6 MB (55.90%)

--- 0907-koko-eating-bananas/solution.swift
+++ 0907-koko-eating-bananas/solution.swift
@@ -1,27 +1,27 @@
 class Solution {
     func minEatingSpeed(_ piles: [Int], _ h: Int) -> Int {
-        var n = piles.count
         var low = 1
         var high = piles.max()!
-        var ans = 0
 
         while low <= high {
-            var mid = (low+high)/2
+            var mid = low+(high-low)/2
             if canEat(mid) {
-                ans = mid
                 high = mid-1
             } else {
                 low = mid+1
             }
         }
 
-        func canEat(_ mid: Int) -> Bool {
+        func canEat(_ val: Int) -> Bool {
             var totalHrs = 0
             for pile in piles {
-                totalHrs += Int(ceil(Double(pile) / Double(mid)))
+                totalHrs += (pile + val - 1) / val
+                if totalHrs > h {
+                    return false
+                }
             }
-            return totalHrs <= h
+            return true
         }
-        return ans
+        return low
     }
 }
