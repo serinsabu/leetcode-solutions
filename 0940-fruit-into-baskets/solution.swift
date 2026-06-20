e44853f943ce719f15708c0e4e4798c0b6257e14
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-06-20 17:30:01 +0000

    Sync LeetCode submission Runtime - 44 ms (53.62%), Memory - 21.3 MB (79.71%)

--- 0940-fruit-into-baskets/solution.swift
+++ 0940-fruit-into-baskets/solution.swift
@@ -1,28 +1,26 @@
 class Solution {
     func totalFruit(_ fruits: [Int]) -> Int {
-        // You don’t assign per position, you assign per type.
-        // [1,2,1] => 1,1 is one type of fruit in one basket, 2 is another type of fruit
-        // Longest subarray with AT MOST 2 distinct elements
-        var ans = 0
         var n = fruits.count
         var map = [Int: Int]()
-        var i = 0
-        var j = 0
+        var i=0
+        var j=0
+        var maxCount = Int.min
 
         while j < n {
             map[fruits[j], default: 0] += 1
-
+            //3-3,1-1
+            // invalid -> shrink
             while map.count > 2 {
-                // shrink
-                map[fruits[i], default: 0] -= 1
-                if map[fruits[i]] == 0 {
+                if map[fruits[i]]! == 1 {
                     map.removeValue(forKey: fruits[i])
+                } else {
+                    map[fruits[i]]! -= 1
                 }
-                i += 1
+                i += 1//
             }
-            ans = max(ans, j-i+1)
-            j += 1
+            maxCount = max(maxCount, j-i+1)
+            j += 1//3
         }
-        return ans
+        return maxCount
     }
 }
