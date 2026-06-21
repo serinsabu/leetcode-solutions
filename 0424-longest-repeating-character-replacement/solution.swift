f5ce0d72cf529c9f920ba14143dae8565ad555f1
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-06-21 18:13:41 +0000

    Sync LeetCode submission Runtime - 34 ms (74.79%), Memory - 21 MB (9.50%)

--- 0424-longest-repeating-character-replacement/solution.swift
+++ 0424-longest-repeating-character-replacement/solution.swift
@@ -1,34 +1,26 @@
 class Solution {
     func characterReplacement(_ s: String, _ k: Int) -> Int {
+        //window size - maxfreq
+        var n = s.count
+        var chars = Array(s)
         var i=0
         var j=0
-        var map=[Character: Int]()
-        var n = s.count
-
-        // You count frequencies inside the current sliding window.
-        var chars = Array(s)
-        var maxFreq = Int.min
+        var map = [Character: Int]()
+        var maxFreq = 0
         var ans = 0
+
         while j < n {
             map[chars[j], default: 0] += 1
             maxFreq = max(maxFreq, map[chars[j]]!)
 
-            // invalid - shrink and remove from map
-            while (j-i+1) - maxFreq > k {
-                if map[chars[i]]! > 1 {
-                    map[chars[i]]! -= 1
-                } else {
-                    map.removeValue(forKey: chars[i])
-                }
+            // invalid
+            while j-i+1 - maxFreq > k {
+                map[chars[i]]! -= 1
                 i += 1
             }
-            // valid window
-            if (j-i+1) - maxFreq <= k {
-                // replace
-                ans = max(j-i+1, ans)
-            }
+            ans = max(ans, j - i + 1)
             j += 1
         }
-    return ans
+        return ans
     }
 }
