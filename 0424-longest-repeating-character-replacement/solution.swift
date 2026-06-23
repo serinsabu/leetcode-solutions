c2f64d4cfde93e29a5d81de4d60d064460481e20
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-06-23 14:11:03 +0000

    Sync LeetCode submission Runtime - 34 ms (74.79%), Memory - 20.5 MB (43.39%)

--- 0424-longest-repeating-character-replacement/solution.swift
+++ 0424-longest-repeating-character-replacement/solution.swift
@@ -1,13 +1,12 @@
 class Solution {
     func characterReplacement(_ s: String, _ k: Int) -> Int {
-        //window size - maxfreq
         var n = s.count
-        var chars = Array(s)
         var i=0
         var j=0
-        var map = [Character: Int]()
+        var chars = Array(s)
         var maxFreq = 0
         var ans = 0
+        var map = [Character: Int]()
 
         while j < n {
             map[chars[j], default: 0] += 1
@@ -18,9 +17,9 @@ class Solution {
                 map[chars[i]]! -= 1
                 i += 1
             }
-            ans = max(ans, j - i + 1)
+            ans = max(ans, j-i+1)
             j += 1
         }
-        return ans
+    return ans
     }
 }
