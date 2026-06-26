d315ed8017e706f568b16f326cc46cf2eeaa1143
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-06-26 18:12:27 +0000

    Sync LeetCode submission Runtime - 94 ms (26.87%), Memory - 20.9 MB (59.47%)

--- 1460-number-of-substrings-containing-all-three-characters/solution.swift
+++ 1460-number-of-substrings-containing-all-three-characters/solution.swift
@@ -1,29 +1,23 @@
 class Solution {
     func numberOfSubstrings(_ s: String) -> Int {
-        var dict = [Character: Int]()//[a:1, b:1, c:1]
-        var n = s.count
-        // dict["a", default: 0] = 1
-        // dict["b", default: 0] = 1
-        // dict["c", default: 0] = 1
-
+        var map = [Character: Int]()
+        var chars = Array(s)
         var i=0
         var j=0
-        var chars = Array(s)
+        var n = s.count
         var count = 0
-        //a-0,b-0,c-0
+
         while j < n {
-            dict[chars[j], default: 0] += 1
-            //a-1,b-1,c-1,a-1
-            // if abc all are present, then start a new window
-            while dict["a", default: 0] > 0 && 
-            dict["b", default: 0] > 0 && 
-            dict["c", default: 0] > 0 {
+            map[chars[j], default: 0] += 1
+
+            while map["a", default: 0] > 0 && 
+                    map["b", default: 0] > 0 && 
+                    map["c", default: 0] > 0 {
+                map[chars[i]]! -= 1
                 count += n-j
-                dict[chars[i], default: 0] -= 1
-                i += 1//3
-                //print(chars[j])
+                i += 1
             }
-            j+=1//4
+            j += 1
         }
         return count
     }
