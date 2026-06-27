e04fda8a2b4607233a24d6b996828a4f798dd86f
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-06-27 09:06:36 +0000

    Sync LeetCode submission Runtime - 4 ms (76.92%), Memory - 19.5 MB (80.77%)

--- 1370-count-number-of-nice-subarrays/solution.swift
+++ 1370-count-number-of-nice-subarrays/solution.swift
@@ -1,32 +1,30 @@
 class Solution {
     func numberOfSubarrays(_ nums: [Int], _ k: Int) -> Int {
-        
-        func atmost(_ val: Int) -> Int {
-            var n = nums.count
+        var n = nums.count
+
+        func atmostk(_ k: Int) -> Int {
             var i=0
             var j=0
             var oddCount = 0
             var count = 0
+
             while j < n {
                 if nums[j]%2 == 1 {
-                    oddCount += 1//3
+                    oddCount += 1 //3
                 }
-
-                while(oddCount > val) {
+                
+                // invalid - shrink
+                while oddCount > k {
                     if nums[i]%2 == 1 {
                         oddCount -= 1
                     }
-                    i += 1
+                    i += 1//1
                 }
-                // evry valid window in exactly k elements
-                if oddCount <= val {
-                    count += j-i+1//1
-                }
-                j += 1
+                count += j-i+1//1
+                j += 1//3
             }
             return count
         }
-
-    return atmost(k) - atmost(k-1)
+    return atmostk(k) - atmostk(k-1)
     }
 }
