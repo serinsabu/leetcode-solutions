f1547b2e438b66aad0e2689e9397fa07bba58ddd
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-05 15:38:24 +0000

    Sync LeetCode submission Runtime - 18 ms (45.56%), Memory - 19.7 MB (51.74%)

--- 0560-subarray-sum-equals-k/solution.swift
+++ 0560-subarray-sum-equals-k/solution.swift
@@ -1,14 +1,19 @@
 class Solution {
     func subarraySum(_ nums: [Int], _ k: Int) -> Int {
-        var sum=0
-        var count=0
-        var map: [Int: Int] = [0:1]
+        var dict = [Int: Int]()
+        var currPrefix = 0
+        var count = 0
+        dict[0] = 1
+        //dict=[0:1,1:3,-1:2]
+
         for num in nums {
-            sum += num//6
-            if let c = map[sum-k] {
-                count += c//2
+            currPrefix += num//1
+            // currPrefixSum - target already exists
+            if let freq = dict[currPrefix - k] {//dict[1-0]
+                // count += 1//4 => [1, -1, 1, -1, 1],k = 0 wrong
+                count += freq//6
             }
-            map[sum, default:0] += 1
+            dict[currPrefix, default: 0] += 1//1->3,-1->2,
         }
         return count
     }
