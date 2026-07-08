d8b371a132ace18b934c03475468e82083f7cf41
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-08 17:03:13 +0000

    Sync LeetCode submission Runtime - 50 ms (27.94%), Memory - 21.8 MB (30.88%)

--- 0940-fruit-into-baskets/solution.swift
+++ 0940-fruit-into-baskets/solution.swift
@@ -1,26 +1,25 @@
 class Solution {
     func totalFruit(_ fruits: [Int]) -> Int {
         var n = fruits.count
-        var map = [Int: Int]()
         var i=0
         var j=0
-        var maxCount = Int.min
+        var dict = [Int:Int]()
+        var maxLength = 0
 
         while j < n {
-            map[fruits[j], default: 0] += 1
-            //3-3,1-1
-            // invalid -> shrink
-            while map.count > 2 {
-                if map[fruits[i]]! == 1 {
-                    map.removeValue(forKey: fruits[i])
+            dict[fruits[j], default: 0] += 1//1-1,2-2
+            // invalid
+            while dict.count > 2 {
+                if dict[fruits[i]]! > 1 {
+                    dict[fruits[i]]! -= 1
                 } else {
-                    map[fruits[i]]! -= 1
+                    dict.removeValue(forKey: fruits[i])
                 }
-                i += 1//
+                i += 1//1
             }
-            maxCount = max(maxCount, j-i+1)
-            j += 1//3
+            maxLength = max(maxLength, j-i+1)//3
+            j += 1
         }
-        return maxCount
+        return maxLength
     }
 }
