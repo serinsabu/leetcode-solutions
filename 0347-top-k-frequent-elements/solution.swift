6aa81bcf23e3e54fba24780e7f458e5f314d541f
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-12 16:08:45 +0000

    Sync LeetCode submission Runtime - 2 ms (57.99%), Memory - 20.7 MB (37.67%)

--- 0347-top-k-frequent-elements/solution.swift
+++ 0347-top-k-frequent-elements/solution.swift
@@ -1,20 +1,22 @@
 class Solution {
     func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
-        var freq:[Int: Int] = [Int:Int]()
-
-        for i in 0..<nums.count {
-            freq[nums[i], default:0] += 1
-        }
-        
-        let sorted = freq.sorted { $0.value > $1.value }
-
+        var n = nums.count
         var ans = [Int]()
-        var index = 0
-        while index < k {
-            ans.append(sorted[index].key)
-            index += 1
+        var dict = [Int: Int]()
+
+        for num in nums {
+            dict[num, default: 0] += 1
+        }
+        //1-3,2-2,3-1
+        let sortedDict = dict.sorted { $0.value > $1.value }
+        //print(sortedDict[0].key)
+        var i=0
+        var k=k
+        while k > 0 {
+            ans.append(sortedDict[i].key)
+            i += 1
+            k -= 1
         }
         return ans
     }
 }
-
