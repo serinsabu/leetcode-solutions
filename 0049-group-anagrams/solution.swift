7c09fc49f39c854225c2bc4ed4a45d96645ca4b8
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-16 18:19:31 +0000

    Sync LeetCode submission Runtime - 10 ms (68.83%), Memory - 21 MB (73.34%)

--- 0049-group-anagrams/solution.swift
+++ 0049-group-anagrams/solution.swift
@@ -1,16 +1,11 @@
 class Solution {
     func groupAnagrams(_ strs: [String]) -> [[String]] {
-        var dict = [[Int]: [String]]()
+        var str = ""
+        var dict = [String: [String]]()
         for word in strs {
-            var freq = Array(repeating: 0, count: 26)
-            for ch in word {
-                let index = Int(ch.asciiValue! - Character("a").asciiValue!)// word = "eat" 
-                // e => 101-97=4, a => 97-97=0, t => 116-97=19
-                freq[index] += 1//freq[4] += 1, freq[0] += 1, freq[19] += 1
-            }
-            dict[freq, default:[]].append(word)
-            //[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0] -> eat, so on..
+            let sortedW = String(word.sorted { $0 < $1 })
+            dict[sortedW, default: []].append(word)
         }
-        return Array(dict.values)
+        return Array((dict.values))
     }
 }
