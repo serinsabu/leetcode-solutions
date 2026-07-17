11a99928466d53651e1ab6bcce35be3464c6d3bb
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-17 18:14:49 +0000

    Sync LeetCode submission Runtime - 41 ms (51.04%), Memory - 20.7 MB (17.71%)

--- 0438-find-all-anagrams-in-a-string/solution.swift
+++ 0438-find-all-anagrams-in-a-string/solution.swift
@@ -1,32 +1,36 @@
 class Solution {
     func findAnagrams(_ s: String, _ p: String) -> [Int] {
         var n = s.count
-        var pArr = Array(repeating:0, count: 26)
-        var sArr = Array(repeating:0, count: 26)
-        var ans = [Int]()
+        var sDict = [Character: Int]()
+        var pDict = [Character: Int]()
+
+        for ch in p {
+            pDict[ch, default: 0] += 1 // a-1,b-1,c-1
+        }
 
         var i=0
         var j=0
-
-        for ch in p {
-            pArr[Int(ch.asciiValue! - Character("a").asciiValue!)] += 1
-        }
+        var k=p.count
         var chars = Array(s)
-        //a-1,b-1,c-1
-        while j < n {
-            sArr[Int(chars[j].asciiValue! - Character("a").asciiValue!)] += 1//c-1,b-1,a-1
+        var ans = [Int]()
 
-            // invalid window
-            if j-i+1 > p.count {
-                //shrink
-                sArr[Int(chars[i].asciiValue! - Character("a").asciiValue!)] -= 1
-                i += 1
+        while j < n {
+            sDict[chars[j], default: 0] += 1
+
+            while j-i+1 > k {
+                // if that character is present in both s and p - SHRINK
+                if sDict[chars[i]]! > 1 {
+                    sDict[chars[i]]! -= 1
+                } else {
+                    sDict.removeValue(forKey: chars[i])
+                }
+                    i += 1
             }
-            // compare the dictionaries
-            if sArr == pArr {
-                ans.append(i)//0
+            // store first occurence
+            if sDict == pDict  {
+                ans.append(i)
             }
-            j += 1//2
+            j += 1
         }
         return ans
     }
