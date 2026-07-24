b2eaebd8c12d0fe63a9b6d6911be0c236fa69e30
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-24 10:55:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20 MB (38.16%)

--- 0547-number-of-provinces/solution.swift
+++ 0547-number-of-provinces/solution.swift
@@ -1,36 +1,27 @@
 class Solution {
     func findCircleNum(_ isConnected: [[Int]]) -> Int {
-        // [[1,1,0],
-        //  [1,1,0],
-        //  [0,0,1]]
-        // 0->{0,1}, 1->{0,1}, 2->{2}
         var n = isConnected.count
         var count = 0
-        var visited = Set<Int>() // to track which cities we have already visited
-    
-        for i in 0..<n {
-            // If city i has not been visited yet,
-            // it means we found a new province
-            if !visited.contains(i) {
+        var visited = Array(repeating: false, count: n)
+
+        //traverse each col [1,1,0]
+        for city in 0..<n {
+            if !visited[city] {
+                dfs(city)
                 count += 1
-                dfs(i) // explore all cities connected to this city
             }
         }
 
-        // DFS function to visit all cities connected to the current city
-        func dfs(_ node: Int) {
-            // Mark this city as visited
-            visited.insert(node)
-            // Check all other cities to see if they are connected to this one
-            for j in 0..<n {
-                // If there is a connection AND that city has not been visited yet
-                if isConnected[node][j] == 1 && !visited.contains(j) {
-                    // Visit that city as well (continue DFS)
-                    dfs(j)
+        func dfs(_ i: Int) {
+            visited[i] = true
+
+            // traverse all the neighbours
+            for neighbour in 0..<n {
+                if !visited[neighbour] && isConnected[i][neighbour] == 1 {
+                    dfs(neighbour)
                 }
             }
         }
-        // After exploring all cities, count will equal the number of provinces
         return count
     }
 }
