12491a0a86cceec631d2331153b417ae51824d48
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-25 12:29:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.7 MB (41.48%)

--- 0048-rotate-image/solution.swift
+++ 0048-rotate-image/solution.swift
@@ -1,34 +1,29 @@
 class Solution {
     func rotate(_ matrix: inout [[Int]]) {
-        // 1 4 7
-        // 2 5 8
-        // 3 6 9
-        // transform rows into columns and vice versa
-        for i in 0..<matrix.count {
-            for j in i..<matrix[i].count {
-                //Swap instead of overwrite
-                // j starts from i → avoids double swapping
-                let temp = matrix[i][j]
+        var m = matrix.count
+        var n = matrix[0].count
+
+        // transpose
+        for i in 0..<m {
+            for j in i..<n {
+                var temp = matrix[i][j]
                 matrix[i][j] = matrix[j][i]
                 matrix[j][i] = temp
             }
         }
-        // reverse each row → 90° rotation
-        // Take each row → reverse it
-        for i in 0..<matrix.count {
+
+        for i in 0..<n {
             reverse(i)
         }
-
-
+        
+        // reverse every row
         func reverse(_ row: Int) {
             var left = 0
-            var right = matrix[row].count-1
-
+            var right = n-1
             while left < right {
-                let temp = matrix[row][left]
+                var temp = matrix[row][left]
                 matrix[row][left] = matrix[row][right]
                 matrix[row][right] = temp
-
                 left += 1
                 right -= 1
             }
