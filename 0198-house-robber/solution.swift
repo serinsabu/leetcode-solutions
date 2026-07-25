8f1abd79c378592342aee51556188897ff15be2e
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-25 16:54:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.8 MB (28.28%)

--- 0198-house-robber/solution.swift
+++ 0198-house-robber/solution.swift
@@ -1,30 +1,30 @@
 class Solution {
     func rob(_ nums: [Int]) -> Int {
         var n = nums.count
+        var ans = 0
+        // when only 1 element 
         if n == 0 {
             return 0
-        }
-
+        }   
         if n == 1 {
             return nums[0]
-        }
-
+        }   
         if n == 2 {
-            return max(nums[1], nums[0])
-        }
-        var first = 0
-        var second = 0
-        var ans = 0
-        first = nums[0]
-        second = max(nums[0], nums[1])
+            return max(nums[0], nums[1])
+        } 
+        var first = nums[0]
+        var second = max(nums[0], nums[1])
         var i = 2
-            while i<n {
-                ans = max(second, first + nums[i])
-                first = second
-                second = ans
-                i += 1
-            }
-        
+        // rob only adjacent houses
+        while i < n {
+            // max(pick current , not pick current)
+            // take house best so far
+            ans = max(nums[i] + first, second)
+            first = second
+            second = ans
+            i += 1
+        }
+        //dp[2]= max(nums[2]+dp[0], dp[1])=max(4,2)
         return ans
     }
 }
