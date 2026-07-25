2ebd08c51aef497424cb2bd89dffbfa48b80c6a9
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-25 15:20:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (43.55%)

---
+++ 0070-climbing-stairs/solution.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        var dp = Array(repeating: 0, count: n+1)
+        
+            dp[0] = 1
+            dp[1] = 1
+        
+        if n > 1 {
+            for i in 2...n {
+                dp[i] = dp[i-1] + dp[i-2]
+            }
+        }
+        return dp[n]
+    }
+}
