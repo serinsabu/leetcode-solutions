1e51397670a9c12a9dffa7a0b9b26e8fadb211ac
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-27 07:05:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.9 MB (16.74%)

--- 0153-find-minimum-in-rotated-sorted-array/solution.swift
+++ 0153-find-minimum-in-rotated-sorted-array/solution.swift
@@ -3,14 +3,15 @@ class Solution {
         var n = nums.count
         var low = 0
         var high = n-1
+        var ans = 0
 
         while low < high {
-            var mid = (low+high)/2
-            if nums[mid] > nums[high] {
-                // if sorted, min is on the right side
-                low = mid+1
-            } else {
+            var mid = low+(high-low)/2
+            // right half is sorted
+            if nums[mid] < nums[high] {
                 high = mid
+            } else {
+                low = mid+1
             }
         }
         return nums[low]
