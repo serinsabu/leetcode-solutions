4ffd5473bbb1f4787d91f5a4292c164469d4a226
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-07-27 10:53:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.2 MB (90.56%)

--- 0056-merge-intervals/solution.swift
+++ 0056-merge-intervals/solution.swift
@@ -1,30 +1,24 @@
 class Solution {
     func merge(_ intervals: [[Int]]) -> [[Int]] {
-        // sort by start time
-        var sorted = intervals.sorted { $0[0] < $1[0] }
-        var s1 = sorted[0][0]//1,8
-        var e1 = sorted[0][1]//3,10
-
         var ans = [[Int]]()
-
-        //case 1 - overlapping
-        for interval in sorted {
-            var s2 = interval[0]//15
-            var e2 = interval[1]//18
-
-            //3 >= 2 - overlapping
-            //3 >= 8 -no
-            //10 >= 15 -no
-            //update end
-            if e1 >= s2 {
-                e1 = max(e1, e2)//6
-            } else {//append when non overlapping
-                ans.append([s1,e1])//[[1,6],[8,10]]
+        let sorted = intervals.sorted { $0[0] < $1[0] }
+        //first interval
+        var s1 = sorted[0][0]//8
+        var e1 = sorted[0][1]//10
+        
+        for interval in 1..<sorted.count {
+            var s2 = sorted[interval][0]//15
+            var e2 = sorted[interval][1]//18
+            // overlap
+            if s2 <= e1 {//2<=3, 8 <= 3 , 15 <= 10
+                e1 = max(e1,e2)//6
+            } else {
+                ans.append([s1,e1])//[[1,6], [8,10]]
                 s1 = s2//15
                 e1 = e2//18
             }
         }
-        ans.append([s1,e1])//[[1,6],[8,10],[15,18]]
-    return ans
+        ans.append([s1,e1])
+        return ans
     }
 }
