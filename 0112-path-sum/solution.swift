99988d1ad51fb3e64f1a5cc33512392f96e92348
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-08-13 16:50:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.1 MB (44.78%)

---
+++ 0112-path-sum/solution.swift
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+        guard let root = root else {
+            return false
+        }
+        return solve(root, targetSum)
+    }
+
+    func solve(_ node: TreeNode?, _ targetSum: Int) -> Bool {
+        var targetSum = targetSum
+        guard let node = node else {
+            return false
+        }
+        targetSum -= node.val
+        // leaf node and remaining == 0
+        if node.left == nil && node.right == nil {
+            return targetSum == 0
+        }
+        let left = solve(node.left, targetSum)
+        let right = solve(node.right, targetSum)
+        return (left || right)
+    }
+}
