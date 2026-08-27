6d3183ab7af42f63d80536b38a4de0d042a5662b
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-08-27 15:11:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.2 MB (18.12%)

--- 0078-subsets/solution.swift
+++ 0078-subsets/solution.swift
@@ -1,57 +1,36 @@
 class Solution {
     func subsets(_ nums: [Int]) -> [[Int]] {
         var n = nums.count
-        var path = [Int]()
-        var result = [[Int]]()
+        var res = [Int]()
+        var ans = [[Int]]()
 
-        /*        current element
-                      |
-              ┌───────┴───────┐
-              ↓               ↓
-            TAKE          DON'T TAKE
-              |               |
-           append             |
-              |               |
-           recurse          recurse
-              |
-          removeLast
-        */
-
-        // Start at index 0 with an empty path.
         solve(0)
 
         func solve(_ index: Int) {
-            // base case
-            // We have processed every element.
-            // Whatever is currently in path is one complete subset.
-            if index == nums.count {
-                result.append(path)
-                return
-            }
+            // You’re only adding res to ans when index == n
+            // But with the for-loop subset approach, every current res is a valid subset.
+            // so commenting index == n
+            // if index == n {
+                ans.append(res)
+                // return
+            // }
+            // at every number
+            // take / skip
+            // for loop does going to every number
+            // choose 1, choose 2 
+            for i in index..<nums.count {
+                // choose
+                res.append(nums[i])
 
-            // ─────────────────────────────
-            // CHOICE 1: TAKE nums[index]
-            // ─────────────────────────────
-            // Choose the current number.
-            path.append(nums[index])
-            // Move to the next index and explore
-            // every possible subset after taking this number.
-            solve(index+1)
-            // ─────────────────────────────
-            // BACKTRACK
-            // ─────────────────────────────
-            // Undo our previous choice.
-            // This brings path back to the state it was in
-            // before we chose nums[index].
-            path.removeLast()
-            // ─────────────────────────────
-            // CHOICE 2: DON'T TAKE nums[index]
-            // ─────────────────────────────
-            // Don't include the current number.
-            // Move to the next index and explore all possibilities
-            // without this number.
-            solve(index+1)
+                // explore choices after i
+                solve(i+1)
+
+                // unchoose
+                res.removeLast()
+            }
+            // skip
+            // solve(index+1)
         }
-        return result
+        return ans
     }
 }
