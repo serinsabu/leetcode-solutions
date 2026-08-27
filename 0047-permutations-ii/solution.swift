b705d540261d27cb1e8cf8cf88ebee565909a546
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-08-27 16:25:45 +0000

    Sync LeetCode submission Runtime - 2 ms (50.00%), Memory - 19.7 MB (66.18%)

--- 0047-permutations-ii/solution.swift
+++ 0047-permutations-ii/solution.swift
@@ -1,34 +1,38 @@
 class Solution {
     func permuteUnique(_ nums: [Int]) -> [[Int]] {
+        let nums = nums.sorted()
         var n = nums.count
-        var path = [Int]()
-        var result = [[Int]]()
-        // var nums = nums
-        // nums.sort()
+        var res = [Int]()
+        var ans = [[Int]]()
         var used = Array(repeating: false, count: n)
+
         solve()
 
         func solve() {
-            if path.count == n {
-                if !result.contains(path) {
-                    result.append(path)
-                }
+            // only allows n number of elements
+            if res.count == n {
+                ans.append(res)
                 return
             }
 
+            // for loop from 0 because [1,2] , [2,1] is both valid
+            // choose 1 -> choose 2 -> [1,2] and also 
+            // choose 2 -> choose 1 -> [2,1]
             for i in 0..<n {
                 if used[i] {
                     continue
                 }
-                path.append(nums[i])
+                // skip duplicates
+                if i > 0 && nums[i] == nums[i-1] && !used[i-1] {
+                    continue
+                }
                 used[i] = true
-
+                res.append(nums[i])
                 solve()
-
-                path.removeLast()
+                res.removeLast()
                 used[i] = false
             }
         }
-        return result
+        return ans
     }
 }
