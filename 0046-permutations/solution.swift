2712f9ed47c51fb681d23609102bfeffd1788174
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-08-27 16:20:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (86.47%)

--- 0046-permutations/solution.swift
+++ 0046-permutations/solution.swift
@@ -1,39 +1,33 @@
 class Solution {
     func permute(_ nums: [Int]) -> [[Int]] {
         var n = nums.count
-        var path = [Int]()
-        var result = [[Int]]()
-
+        var res = [Int]()
+        var ans = [[Int]]()
         var used = Array(repeating: false, count: n)
+
         solve()
-        
+
         func solve() {
-            // BASE CASE:
-            // We have chosen all elements
-            if path.count == n {
-                result.append(path)
+            // only allows n number of elements
+            if res.count == n {
+                ans.append(res)
                 return
             }
 
-            // Try every element for the current position
+            // for loop from 0 because [1,2] , [2,1] is both valid
+            // choose 1 -> choose 2 -> [1,2] and also 
+            // choose 2 -> choose 1 -> [2,1]
             for i in 0..<n {
-                // If nums[i] is already used in this path,
-                // we cannot use it again
                 if used[i] {
                     continue
                 }
-                // CHOOSE
-                path.append(nums[i])
                 used[i] = true
-
-                // EXPLORE
+                res.append(nums[i])
                 solve()
-
-                // BACKTRACK / UNDO
-                path.removeLast()
+                res.removeLast()
                 used[i] = false
             }
         }
-        return result
+        return ans
     }
 }
