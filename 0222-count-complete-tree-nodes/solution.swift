606fb4e0bf7a3cf26c258ef4e18b39c01bc9780d
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-08-30 17:55:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.8 MB (13.79%)

--- 0222-count-complete-tree-nodes/solution.swift
+++ 0222-count-complete-tree-nodes/solution.swift
@@ -13,24 +13,86 @@
  *     }
  * }
  */
+
+ /*
+
+ solve(node)
+    ↓
+find leftmost height
+find rightmost height
+    ↓
+same?
+ /   \
+
+YES   NO
+ ↓     ↓
+2^h-1 solve(left) + solve(right) + 1
+
+ */
 class Solution {
     func countNodes(_ root: TreeNode?) -> Int {
-        if root == nil {
+        guard let root = root else {
             return 0
         }
         return solve(root)
     }
-
+    // you don’t need to calculate the height of the entire tree separately first.
+    // You don’t need to visit every node in that subtree.
+    // if leftHeight != rightHeight
+    //  The subtree isn’t perfect, so recurse into the left and right children.
+    /*
+    calculates the height of the whole subtree, which means it visits both sides.
+    But for Count Complete Tree Nodes, we specifically want:
+    “How deep is the leftmost path?”
+    So you only follow one direction.
+    */
     func solve(_ node: TreeNode?) -> Int {
-        // Base Case
-        if node == nil {
+        guard let node = node else {
             return 0
         }
-        // Ask Left Child
-        let left = solve(node?.left)
-        // Ask Right Child
-        let right = solve(node?.right)
-        // Combine Left + Right + Current Node
-        return left+right+1
+
+        let lh = leftHeight(node)
+        let rh = rightHeight(node)
+        var left = 0
+        var right = 0
+
+        // perfect tree
+        if lh == rh {
+            // number of nodes = 2^height - 1
+            return Int(pow(2.0, Double(lh))) - 1
+        } else { // complete tree
+            // recurse
+            left = solve(node.left)
+            right = solve(node.right)
+        }
+        return 1+left+right
+    }
+    //  your leftHeight and rightHeight functions each take O(log n) time because a complete tree has height O(log n).
+    func leftHeight(_ node: TreeNode?) -> Int {
+        guard let node = node else {
+            return 0
+        }
+
+        var current: TreeNode? = node
+        var height = 0
+        while current != nil {
+            height += 1
+            current = current!.left
+        }
+        return height
+    }
+
+    func rightHeight(_ node: TreeNode?) -> Int {
+        guard let node = node else {
+            return 0
+        }
+
+        var current: TreeNode? = node
+        var height = 0
+        while current != nil {
+            height += 1
+            current = current!.right
+        }
+        return height
     }
 }
