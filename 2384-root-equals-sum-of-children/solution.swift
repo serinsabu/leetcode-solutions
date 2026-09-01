1902b8a8d8efa2c9dbf890b63057df5336098148
Serin Sabu <56440848+serinsabu@users.noreply.github.com>
2026-09-01 16:29:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (80.00%)

---
+++ 2384-root-equals-sum-of-children/solution.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func checkTree(_ root: TreeNode?) -> Bool {
+        guard let root = root else {
+            return false
+        }
+        // root.left?.val + root.right?.val => swift does not allow
+        // means 10 + nil = ??? There is no meaningful integer answer.
+
+        guard let left = root.left?.val, let right = root.right?.val else {
+            return false
+        }
+
+        return root.val == left + right
+    }
+}
